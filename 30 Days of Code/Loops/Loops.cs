443525c7a048be90720cd11b60824cd25e8b1963
aggourav <gourav.agarwal@qualfon.com>
2020-09-05 12:20:10 +0530

    added code for loops

---
+++ 30 Days of Code/Loops/Loops.cs
@@ -0,0 +1,29 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        if (n >= 2 && n <= 20)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} x {i} = {n * i}");
+            }
+        }
+    }
+}
